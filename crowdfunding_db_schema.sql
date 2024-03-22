6fe41944fd284751557bae9592a46f035ce24d38
Idalia <angelie.wanner@gmail.com>
2024-03-21 19:24:32 -0500

    updated schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,66 +2,59 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 --drop campaign table if already exists
-DROP TABLE Campaign
-
---create campaign table
-CREATE TABLE "Campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR   NOT NULL,
-    "description" VARCHAR   NOT NULL,
-    "goal" FLOAT   NOT NULL,
-    "pledged" FLOAT   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
-    "backers_count" INT   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
-
+DROP TABLE "Campaign";
 --drop contacts table if already exists
-DROP TABLE Contacts
-
---create contacts table
-CREATE TABLE "Contacts" (
-    "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
-);
-
+DROP TABLE "Contacts";
 --drop category table if already exists
-DROP TABLE Category
+DROP TABLE "Category";
+--drop subcategory table if already exists
+DROP TABLE "Subcategory";
 
 --create category table
 CREATE TABLE "Category" (
-    "category_id" VARCHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
-        "category_id"
-     )
+    "category_id" VARCHAR NOT NULL,
+    "category" VARCHAR NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY ("category_id")
 );
 
---drop subcategory table if already exists
-DROP TABLE Subcategory
 
 --create subcategory table
 CREATE TABLE "Subcategory" (
     "subcategory_id" VARCHAR   NOT NULL,
     "subcategory" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY ("subcategory_id")
 );
+
+--create campaign table
+CREATE TABLE "Campaign" (
+    "cf_id" INT NOT NULL,
+    "contact_id" INT NOT NULL,
+    "company_name" VARCHAR NOT NULL,
+    "description" VARCHAR NOT NULL,
+    "goal" FLOAT NOT NULL,
+    "pledged" FLOAT NOT NULL,
+    "outcome" VARCHAR NOT NULL,
+    "backers_count" INT NOT NULL,
+    "country" VARCHAR NOT NULL,
+    "currency" VARCHAR NOT NULL,
+    "launched_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "category_id" VARCHAR NOT NULL,
+    "subcategory_id" VARCHAR NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY ("cf_id")
+);
+
+
+--create contacts table
+CREATE TABLE "Contacts" (
+    "contact_id" INT NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY ("contact_id")
+);
+
+
 
 -- alter tables to relate to each other using foreign keys
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")

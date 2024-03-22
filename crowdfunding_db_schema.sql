bf2bb956fa4cf29b79b890c325f0d4fefa93d774
Idalia <angelie.wanner@gmail.com>
2024-03-21 20:39:55 -0500

    updated crowdfunding_db schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -12,16 +12,16 @@ DROP TABLE IF EXISTS "Subcategory";
 
 --create category table
 CREATE TABLE "Category" (
-    "category_id" VARCHAR NOT NULL,
-    "category" VARCHAR NOT NULL,
+    "category_id" VARCHAR(50) NOT NULL,
+    "category" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY ("category_id")
 );
 
 
 --create subcategory table
 CREATE TABLE "Subcategory" (
-    "subcategory_id" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR(50) NOT NULL,
+    "subcategory" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY ("subcategory_id")
 );
 
@@ -29,18 +29,18 @@ CREATE TABLE "Subcategory" (
 CREATE TABLE "Campaign" (
     "cf_id" INT NOT NULL,
     "contact_id" INT NOT NULL,
-    "company_name" VARCHAR NOT NULL,
-    "description" VARCHAR NOT NULL,
+    "company_name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(250) NOT NULL,
     "goal" FLOAT NOT NULL,
     "pledged" FLOAT NOT NULL,
-    "outcome" VARCHAR NOT NULL,
+    "outcome" VARCHAR(50) NOT NULL,
     "backers_count" INT NOT NULL,
-    "country" VARCHAR NOT NULL,
-    "currency" VARCHAR NOT NULL,
+    "country" VARCHAR(50) NOT NULL,
+    "currency" VARCHAR(50) NOT NULL,
     "launched_date" DATE NOT NULL,
     "end_date" DATE NOT NULL,
-    "category_id" VARCHAR NOT NULL,
-    "subcategory_id" VARCHAR NOT NULL,
+    "category_id" VARCHAR(50) NOT NULL,
+    "subcategory_id" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY ("cf_id")
 );
 
@@ -48,9 +48,9 @@ CREATE TABLE "Campaign" (
 --create contacts table
 CREATE TABLE "Contacts" (
     "contact_id" INT NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "email" VARCHAR NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_Contacts" PRIMARY KEY ("contact_id")
 );
 
